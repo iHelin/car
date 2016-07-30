0e62cd8e888bb24b99c14c56d20de529d3094aa4
iHelin <ihelin@outlook.com>
2016-07-30 17:59:09 +0800

    项目结构修改

--- src/main/java/com/ihelin/car/controller/IndexConroller.java
+++ src/main/java/com/ihelin/car/controller/IndexConroller.java
@@ -1,10 +1,14 @@
 package com.ihelin.car.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexConroller extends BaseController {
+	
+	private static final Log logger = LogFactory.getLog(IndexConroller.class);
 
 	@RequestMapping("/")
 	public String root() {
@@ -13,6 +17,12 @@ public class IndexConroller extends BaseController {
 
 	@RequestMapping(value = "index")
 	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "test")
+	public String test() {
+		logger.info("呵呵");
 		return "index";
 	}
 
